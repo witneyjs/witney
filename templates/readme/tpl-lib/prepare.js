2f72110aa23e875206f5cf6e5034d9d7b2afd278
Katja Lutz <mail@katjalutz.ch>
2018-11-04 18:34:33 +0100

    [FEATURE] Switch readme badge rendering from html/css to svg

--- templates/readme/tpl-lib/prepare.js
+++ templates/readme/tpl-lib/prepare.js
@@ -65,6 +65,44 @@ const getFailedTestsCount = function(nameSpaceId) {
     return Number.parseInt(failedTestsCount);
 }
 
+const renderBadge = function({ title, titleColor, titleTextColor, body, bodyColor, bodyTextColor }) {
+    const charWidth = 8;
+    const columnSpacing = 7;
+
+    const titleWidth = title.length * charWidth;
+    const bodyWidth = body.length * charWidth;
+
+    const titleColumnWidth = titleWidth + columnSpacing * 2;
+    const bodyColumnWidth = bodyWidth + columnSpacing * 2;
+
+    const titleColumnX = 0;
+    const bodyColumnX = titleColumnWidth;
+
+    const titleX = columnSpacing;
+    const bodyX = titleColumnWidth + titleX;
+
+    const badge = `
+<svg xmlns="http://www.w3.org/2000/svg" width="${titleColumnWidth + bodyColumnWidth}" height="20">
+    <linearGradient id="a" x2="0" y2="100%">
+        <stop offset="0" stop-color="#bbb" stop-opacity=".1" />
+        <stop offset="1" stop-opacity=".1" />
+    </linearGradient>
+    <rect rx="3" width="${titleColumnWidth + bodyColumnWidth}" height="20" fill="${titleColor}" />
+    <rect rx="3" x="${bodyColumnX}" width="${bodyColumnWidth}" height="20" fill="${bodyColor}" />
+    <path fill="${bodyColor}" d="M${bodyColumnX} 0h4v20h-4z" />
+    <rect rx="3" width="${titleColumnWidth + bodyColumnWidth}" height="20" fill="url(#a)" />
+    <g fill="${titleTextColor}" font-family="monospace" font-size="12" font-weight="bold">
+        <text x="${titleX}" y="15" fill="#010101" fill-opacity=".5">${title}</text>
+        <text x="${titleX}" y="14">${title}</text>
+    </g>
+    <g fill="${bodyTextColor}" font-family="monospace" font-size="12" font-weight="bold">
+        <text x="${bodyX}" y="15" fill="#010101" fill-opacity=".5">${body}</text>
+        <text x="${bodyX}" y="14">${body}</text>
+    </g>
+</svg>`
+    return badge.trim().replace(/\n/g,'');
+}
+
 const prepare = function(hoppla) {
     const projectPkgFile = paths.project('package.json');
     hoppla.input.pkg = require(projectPkgFile);
@@ -73,6 +111,8 @@ const prepare = function(hoppla) {
 
     hoppla.input.coverage = Math.round(testsSummary.coverage);
     hoppla.input.failedTestsCount = testsSummary.failedTestsCount;
+
+    hoppla.input.renderBadge = renderBadge;
 }
 
 module.exports = prepare;
