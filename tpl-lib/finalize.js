4ecfbe0439379785b2016c5a4711a7e1d8c97474
Katja Lutz <mail@katjalutz.ch>
2018-11-17 15:53:17 +0100

    [FEATURE] Upgraded hoppla to 0.6.0, added witney tpl demo finalizer

---
+++ tpl-lib/finalize.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const { execSync } = require('child_process');
+
+const finalize = function(hoppla) {
+    if (hoppla.input.type === 'demo') {
+        execSync(`yarn install --cwd "${hoppla.destination}"`, { stdio: 'inherit' });
+
+        const nameSpaceTemplate = path.join(hoppla.destination, 'templates', 'namespace');
+
+        const config = {
+            template: nameSpaceTemplate,
+            destination: hoppla.destination,
+        }
+
+        Promise.resolve()
+            .then(() => {
+                return hoppla.call({
+                    ...config,
+                    input: {
+                        id: 'front'
+                    }
+                });
+            })
+            .then(() => {
+                return hoppla.call({
+                    ...config,
+                    input: {
+                        id: 'back',
+                        isNode: true
+                    }
+                });
+            })
+    }
+}
+
+module.exports = finalize;
