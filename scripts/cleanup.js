e0620ad50a4cd392be2e786115e190aa595724a8
Katja Lutz <mail@katjalutz.ch>
2018-11-21 15:52:48 +0100

    [FEATURE] Implemented  basic cleanup script

--- scripts/cleanup.js
+++ scripts/cleanup.js
@@ -1,2 +1,8 @@
-// TODO: Create this script
-// TODO: Delete test tmp file
+#!/usr/bin/env node
+
+const paths = require('common').paths;
+const shell = require('shelljs');
+
+shell.rm('-rf', paths.tmp('*'));
+shell.rm('-rf', paths.tmp('.*'));
+shell.echo('-n', '').to(paths.tmp('.gitkeep'))
