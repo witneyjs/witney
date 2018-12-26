a47921cf0997157dd6009ad230b5dad0cc8603e6
Katja Lutz <mail@katjalutz.ch>
2018-12-26 18:35:26 +0100

    [FEATURE] Docs script: Added info for gh-pages

--- scripts/docs.js
+++ scripts/docs.js
@@ -2,4 +2,9 @@
 
 const shell = require("shelljs");
 const paths = require("common").paths;
+const pino = require("pino")({ prettyPrint: { forceColor: true } });
+
 shell.exec(`npx mallery ${paths.config("mallery.config.js")}`);
+pino.info(
+  'Manual docs action: Commit and run: "git subtree push --prefix static/docs origin gh-pages"'
+);
