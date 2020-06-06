f99a36340ece05021cb448abdb707888d7b67d98
Katja Lutz <mail@katjalutz.ch>
2020-06-06 17:18:24 +0200

    feat: implement create-type-proxies script

--- scripts/before-publish.js
+++ scripts/before-publish.js
@@ -4,6 +4,7 @@ const { spawnScript } = require("./lib");
 
 spawnScript("reformat");
 spawnScript("build");
+spawnScript("create-type-proxies");
 spawnScript("coverage");
 
 console.log("Manual action: `npx standard-version -a -r major|minor|patch`");
